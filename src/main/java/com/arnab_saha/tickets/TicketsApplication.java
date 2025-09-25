795f766d1a5e648f9afde380aa6117ed91682878
Arnab Saha <arnab@Arnabs-MacBook-Pro.local>
2025-09-25 13:00:59 -0700

    Init

---
+++ src/main/java/com/arnab_saha/tickets/TicketsApplication.java
@@ -0,0 +1,13 @@
+package com.arnab_saha.tickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arnab_saha/tickets/TicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arnab_saha.tickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
